2d779b2b1bea29d9c56d13cbc2b7b5b354238c5f
Darshan T.C <78899616+DarshanTejur@users.noreply.github.com>
2023-07-08 15:21:20 +0530

    project_V.py

--- project_V.py
+++ project_V.py
@@ -1,3 +1,23 @@
+"""
+1. The code imports the necessary modules: `socket` for creating network sockets and `sys` for system-specific parameters and functions.
+
+2. The `scan_ports()` function is defined to scan a range of ports on a target host:
+   - It iterates over the range of ports from the start port to the end port.
+   - For each port, it creates a socket object and sets a timeout of 1 second.
+   - It tries to connect to the target host and port using `sock.connect_ex()`.
+   - If the connection is successful (return value of 0), it prints that the port is open. Otherwise, it prints that the port is closed.
+   - It closes the socket connection.
+
+3. The example usage section sets the target host IP address, start port, and end port.
+
+4. It prints a message indicating the range of ports being scanned and the target host.
+
+5. The `scan_ports()` function is called with the provided parameters.
+
+The code scans the specified range of ports on the target host by attempting to establish a connection to each port. It then prints whether each port is open or closed based on the connection result.
+
+"""
+
 import socket
 import sys
 
